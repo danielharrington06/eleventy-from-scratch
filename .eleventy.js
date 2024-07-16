708fba892b670db9ad1687ba03ab43aaf66143ae
Daniel Harrington <danielharrington06@outlook.com>
2024-07-16 11:47:12 +0100

    Lesson 12 actually complete

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,18 @@
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js')
+
 module.exports = config => {
+
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/');
-  const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js')
+
   // Returns work items, sorted by display order
   config.addCollection('work', collection => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
@@ -13,6 +24,8 @@ module.exports = config => {
       x => x.data.featured
     );
   });
+
+  // Returns a collection of blog posts in reverse date order
   config.addCollection('blog', collection => {
     return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
   });
